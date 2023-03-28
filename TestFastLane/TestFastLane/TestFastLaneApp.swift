980eb8b2e9de826a77262853851a13ebd7fd6b29
piggy.park <www.piggy.park@groundx.xyz>
2023-03-28 13:18:48 +0900

    init proj

---
+++ TestFastLane/TestFastLane/TestFastLaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestFastLaneApp.swift
+//  TestFastLane
+//
+//  Created by 박진섭 on 2023/03/28.
+//
+
+import SwiftUI
+
+@main
+struct TestFastLaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
